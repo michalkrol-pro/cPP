f048b2dfce86801fdcd3f400b30c6c22cf56592b
Michal Krol <mkrol1992@yahoo.com>
2020-08-08 14:41:36 +0200

    commit z macbooka

---
+++ src/odcinek10/git.java
@@ -0,0 +1,8 @@
+package odcinek10;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("Pisze to na macbooku, a cały projekt sklonowałem z gitHuba");
+        System.out.println("chce go zapisać, zakomitować, zapuschowac a na iMaku zapulować i odczytać");
+    }
+}
