44eae37b7e08f96aee48405e906d58658e6c50ca
michalkrol-pro <62615562+michalkrol-pro@users.noreply.github.com>
2020-08-08 14:50:54 +0200

    commit z iMaca

--- src/odcinek10/git.java
+++ src/odcinek10/git.java
@@ -4,5 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("Pisze to na macbooku, a cały projekt sklonowałem z gitHuba");
         System.out.println("chce go zapisać, zakomitować, zapuschowac a na iMaku zapulować i odczytać");
+        System.out.println(" działa:D widzę na iMacu");
     }
 }
